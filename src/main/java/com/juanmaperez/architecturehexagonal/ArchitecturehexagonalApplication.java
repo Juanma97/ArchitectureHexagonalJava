40aeab39b78b614ecad4985c87ce8fe3efd69103
Juan Manuel Perez Toro <juanmaperez@MacBook-Pro-de-Juan.local>
2021-06-14 20:26:33 +0100

    First commit

---
+++ src/main/java/com/juanmaperez/architecturehexagonal/ArchitecturehexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.juanmaperez.architecturehexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitecturehexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchitecturehexagonalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juanmaperez/architecturehexagonal/ArchitecturehexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanmaperez.architecturehexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitecturehexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
